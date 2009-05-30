c2f2cd3a0eace38b4af4b1a80828db7f253ba15d
ragaskar <ragaskar@gmail.com>
2009-05-29 22:30:32 -0700

    install mods

--- install.rb
+++ install.rb
@@ -1,4 +1,4 @@
 Dir.mkdir('spec') unless File.exist?('spec')
-Dir.mkdir('spec/javascipt') unless File.exist?('spec/javascript')
+Dir.mkdir('spec/javascript') unless File.exist?('spec/javascript')
 File.copy('vendor/plugins/jazzhands/templates/jasmine_spec.rb', 'spec/javascript/')
 File.copy('vendor/plugins/jazzhands/templates/example_test.js', 'spec/javascript/')
